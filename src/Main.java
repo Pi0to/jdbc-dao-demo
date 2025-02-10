d6866e52e04350dada414f0a596841d88598d7c4
Théo Pioto <theopioto@gmail.com>
2025-02-10 08:22:18 -0300

    upload project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Department obj = new Department(1, "Books");
+
+        System.out.println(obj);
     }
 }
